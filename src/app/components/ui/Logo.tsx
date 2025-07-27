c1f3f633b3d329ad101a0ab4fd137cb06334e179
BirukW-23 <birukwondmeneh@gmail.com>
2025-07-27 22:33:17 +0300

    Feat: Add feature which is forgot password

--- src/app/components/ui/Logo.tsx
+++ src/app/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 import logoimage from "../../../../public/Header.jpg"
 function Logo() {
   return (
-    <div className='bg-[#242E3A] py-6'>
+    <div className='bg-[#242E3A] py-8'>
       <Image 
       src={logoimage}
       alt='Logo slider'
