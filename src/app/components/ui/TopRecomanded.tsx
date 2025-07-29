5501c0c4a652fe0ecdea19983ad9e713470d0165
BirukW-23 <birukwondmeneh@gmail.com>
2025-07-29 14:52:14 +0300

    Fix the top recomanded page

---
+++ src/app/components/ui/TopRecomanded.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import PlaceNameCard from "@/app/components/ui/Card";
+
+const topRecommendedData = [
+  {
+    id: "1",
+    name: "Place name",
+    location: "Location",
+    rating: 4.5,
+    reviews: "400",
+    image: "/image1.jpg",
+  },
+  {
+    id: "2",
+    name: "Place name",
+    location: "Location",
+    rating: 4.5,
+    reviews: "400",
+    image: "/image2.jpg",
+  },
+  {
+    id: "3",
+    name: "Place name",
+    location: "Location",
+    rating: 4.5,
+    reviews: "400",
+    image: "/fox.jpg",
+  },
+];
+
+function TopRecommended() {
+  return (
+    <div className="w-full py-25" style={{ fontFamily: "'Century Gothic', sans-serif", fontWeight: 300 }}>
+      <div className="flex gap-2 justify-between">
+        <h2 className="text-lg  text-[#959494]  font-bold mb-4 px-8">
+          Top Recommended
+        </h2>
+        <span className="pr-6 text-[#959494]">See all</span>
+      </div>
+      <div className="overflow-x-auto px-7">
+        <div className="flex gap-4 pb-4 snap-x snap-mandatory overflow-x-auto scroll-smooth">
+          {topRecommendedData.map((item) => (
+            <div key={item.id} className="snap-start shrink-0">
+              <PlaceNameCard
+                image={item.image}
+                name={item.name}
+                location={item.location}
+                percentage={item.rating}
+                value={item.reviews}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default TopRecommended;
